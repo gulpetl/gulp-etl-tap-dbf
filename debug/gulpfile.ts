bc8e5d30c6d95eb5b7d830a1c6150f6e79c1e8b4
DSilwal <deepen.silwal@ttu.edu>
2019-06-12 11:02:17 -0500

    final commit

--- debug/gulpfile.ts
+++ debug/gulpfile.ts
@@ -1,6 +1,5 @@
 let gulp = require('gulp')
 import { tapDbf } from '../src/plugin'
-
 import * as loglevel from 'loglevel'
 const log = loglevel.getLogger('gulpfile')
 log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
@@ -10,15 +9,11 @@ log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
 
 import * as rename from 'gulp-rename'
 const errorHandler = require('gulp-error-handle'); // handle all errors in one handler, but still stop the stream if there are errors
-
 const pkginfo = require('pkginfo')(module); // project package.json info into module.exports
 const PLUGIN_NAME = module.exports.name;
-
 import Vinyl = require('vinyl') 
 
-
 let gulpBufferMode = false;
-
 function switchToBuffer(callback: any) {
   gulpBufferMode = true;
 
@@ -27,8 +22,7 @@ function switchToBuffer(callback: any) {
 
 function runTapDbf(callback: any) {
   log.info('gulp task starting for ' + PLUGIN_NAME)
-
-  return gulp.src('../testdata/*.DBF',{buffer:false})
+  return gulp.src('../testdata/*.DBF',{buffer: gulpBufferMode})
     .pipe(errorHandler(function(err:any) {
       log.error('Error: ' + err)
       callback(err)
@@ -36,7 +30,7 @@ function runTapDbf(callback: any) {
     .on('data', function (file:Vinyl) {
       log.info('Starting processing on ' + file.basename)
     })    
-    .pipe(tapDbf())
+    .pipe(tapDbf({deleted: false}))
     .pipe(rename({
       extname: ".ndjson",
     }))      
@@ -66,4 +60,4 @@ export function csvParseWithoutGulp(callback: any) {
 }*/
 
 exports.default = gulp.series(runTapDbf)
-//exports.runTapCsvBuffer = gulp.series(switchToBuffer, runTapCsv)
+exports.runTapCsvBuffer = gulp.series(switchToBuffer, runTapDbf)
--- src/plugin.ts
+++ src/plugin.ts
@@ -4,10 +4,11 @@ import PluginError = require('plugin-error');
 const pkginfo = require('pkginfo')(module); // project package.json info into module.exports
 const PLUGIN_NAME = module.exports.name;
 import * as loglevel from 'loglevel'
+//import { read, createReadStream } from 'fs';
 const log = loglevel.getLogger(PLUGIN_NAME) // get a logger instance based on the project name
-log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
-const fs = require('fs');
-
+//log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
+const toStream = require('buffer-to-stream');
+const getStream = require('get-stream');
 const YADBF = require('yadbf')
 
 /** wrap incoming recordObject in a Singer RECORD Message object*/
@@ -15,31 +16,23 @@ function createRecord(recordObject:Object, streamName: string) : any {
   return {type:"RECORD", stream:streamName, record:recordObject}
 }
 
+
 /* This is a gulp-etl plugin. It is compliant with best practices for Gulp plugins (see
 https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/guidelines.md#what-does-a-good-plugin-look-like ),
 and like all gulp-etl plugins it accepts a configObj as its first parameter */
-export function tapDbf() {
-  //if (!configObj) configObj = {}
-  //if (!configObj.columns) configObj.columns = true // we don't allow false for columns; it results in arrays instead of objects for each record
-
+export function tapDbf(configObj: any) {
+ 
   // creating a stream through which each file will pass - a new instance will be created and invoked for each file 
   // see https://stackoverflow.com/a/52432089/5578474 for a note on the "this" param
   const strm = through2.obj(function (this: any, file: Vinyl, encoding: string, cb: Function) {
     const self = this
     let returnErr: any = null
-    //const parser = YADBF()
+   
 
     // post-process line object
     const handleLine = (lineObj: any, _streamName : string): object | null => {
-      //if (parser.options.raw || parser.options.info) {
         let newObj = createRecord(lineObj, _streamName)
-       // if (lineObj.raw) newObj.raw = lineObj.raw
-       // if (lineObj.info) newObj.info = lineObj.info
         lineObj = newObj
-      //}
-      //else {
-        //lineObj = createRecord(lineObj, _streamName)
-      //}
       return lineObj
     }
 
@@ -48,7 +41,7 @@ export function tapDbf() {
 
       let transformer = through2.obj(); // new transform stream, in object mode
   
-      // transformer is designed to follow csv-parse, which emits objects, so dataObj is an Object. We will finish by converting dataObj to a text line
+      // transformer is designed to follow yadbf, which emits objects, so dataObj is an Object. We will finish by converting dataObj to a text line
       transformer._transform = function (dataObj: Object, encoding: string, callback: Function) {
         let returnErr: any = null
         try {
@@ -56,15 +49,14 @@ export function tapDbf() {
           if (handledObj) {
             let handledLine = JSON.stringify(handledObj)
             log.debug(handledLine)
-            this.push(handledLine + '\n');
+            this.push(handledLine + '\n')
           }
         } catch (err) {
           returnErr = new PluginError(PLUGIN_NAME, err);
         }
-  
+        
         callback(returnErr)
       }
-  
       return transformer
     }
     
@@ -75,53 +67,26 @@ export function tapDbf() {
       // return empty file
       return cb(returnErr, file)
     }
-    /*
+
     else if (file.isBuffer()) {
-      
-      
-
-      (file.contents as Buffer, function(err:any, linesArray : []){
-        // this callback function runs when the parser finishes its work, returning an array parsed lines 
-        
-        let tempLine: any
-        let resultArray = [];
-        // we'll call handleLine on each line
-        for (let dataIdx in linesArray) {
-          try {
-            let lineObj = linesArray[dataIdx]
-            tempLine = handleLine(lineObj, streamName)
-            if (tempLine){
-              let tempStr = JSON.stringify(tempLine)
-              log.debug(tempStr)
-              resultArray.push(tempStr);
-            }
-          } catch (err) {
-            returnErr = new PluginError(PLUGIN_NAME, err);
-          }
+      const readable = toStream(Buffer.from(file.contents))
+      let mystream = readable.pipe(new YADBF())
+      .pipe(newTransformer(streamName))
+      .on('finish', async () => {
+        try {
+            file.contents = await getStream.buffer(mystream);       
         }
-        let data:string = resultArray.join('\n')
-
-        file.contents = Buffer.from(data)
-        
-        // we are done with file processing. Pass the processed file along
-        log.debug('calling callback')    
-        cb(returnErr, file);    
+        catch (err) {
+          console.error(err)
+        }
+        cb(returnErr, file);
       })
-
-    }*/
+    }
     
     else if (file.isStream()) {
       file.contents = file.contents
-        .pipe(new YADBF())
-        /*
-        .on('header', (header: any) => {
-          console.log(`header: ${JSON.stringify(header, null, 2)}`);
-        })
-        .on('data', (record: any) => {
-          console.log(`record: ${JSON.stringify(record, null, 2)}`);
-        })*/
-        
-
+        .pipe(new YADBF(configObj))
+ 
         .on('end', function () {
 
           // DON'T CALL THIS HERE. It MAY work, if the job is small enough. But it needs to be called after the stream is SET UP, not when the streaming is DONE.
@@ -129,7 +94,7 @@ export function tapDbf() {
           // cb(returnErr, file);
           // log.debug('calling callback')    
 
-          log.debug('csv parser is done')
+          log.debug('dbf parser is done')
         })
         // .on('data', function (data:any, err: any) {
         //   log.debug(data)
